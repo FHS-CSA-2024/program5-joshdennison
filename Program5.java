9b4f1b02ccee8f4adecdf56e0c4c9b8529e95a24
joshdennison <jden3.14@gmail.com>
2024-08-20 09:52:16 -0700

    Update Program5.java

--- Program5.java
+++ Program5.java
@@ -1,9 +1,52 @@
 //import stuff here?
 
 //Your code here
-
+public class program5
+{
+    public static void main(String[] args)
+{
+    double rm = 286.0;
+    double rg = 9.0;
+    double kkm=  412.0;
+    double kkg =40.0;
+    double pfm= 361.0;
+    double pfg= 18.0;
+    double bwm =161.0;
+    double bwg =11.0;
+System.out.println("Mushroom cup prix racer average Miles/per Gallon");
+double rmpg1= (int) (((rm/rg)*10)+.5);
+double rmpg2= (double) rmpg1/10;
+System.out.println(" ");
+System.out.print("Royale averaged "+ rmpg2 );
+    System.out.println("m/g");
+    double kkmpg1 = (int) (((kkm/kkg)*10)+.5);
+    double kkmpg2= (double) kkmpg1/10;
+    
+System.out.println(" ");
+System.out.print("Koopa king averaged " + kkmpg2);
+System.out.println("m/g");
+double pfmpg1 = (int) (((pfm/pfg)*10)+.5);
+    double pfmpg2= (double) pfmpg1/10;
+    System.out.println(" ");
+    System.out.println("pipe frame averaged " + pfmpg2+ "m/g");
+    
+    
+    
+    
+    double bwmpg1 = (int) (((bwm/bwg)*10)+.5);
+    double bwmpg2= (double) bwmpg1/10;
+    System.out.println("badwagon averaged "+ bwmpg2+ "m/g");
 //Paste console output below:
 /*
+Mushroom cup prix racer average Miles/per Gallon
+ 
+Royale averaged 31.8m/g
+ 
+Koopa king averaged 10.3m/g
+ 
+pipe frame averaged 20.1m/g
+badwagon averaged 14.6m/g
+
 
 
 */
